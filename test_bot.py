11d0737d372eaf4b2b944147d47a45cf05b58d0b
Eric Chan <ericchw97@hotmail.com>
2022-10-04 22:03:35 +0800

    test update

--- test_bot.py
+++ test_bot.py
@@ -1,4 +1,6 @@
-import os, bot
+import os
+import discord
+import chat
 from discord.ui import Button, View, button
 from dotenv import load_dotenv
 
@@ -6,39 +8,51 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD_ID')
 
-intents = bot.Intents.default()
+intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 
-bot = bot.Bot(debug_guilds=[GUILD], intents=intents) # specify the guild IDs in debug_guilds
+# specify the guild IDs in debug_guilds
+bot = discord.Bot(debug_guilds=[GUILD], intents=intents)
 
 # since global slash commands can take up to an hour to register,
 # we need to limit the guilds for testing purposes
 
+
 class MyView(View):
-    @button(label="Button 1", style=bot.ButtonStyle.blurple, emoji="😊")
+    @button(label="Button 1", style=discord.ButtonStyle.blurple, emoji="😊")
     async def callback1(self, button, interaction):
         await interaction.response.edit_message(content=f"Hi from Button 1", view=self)
         await interaction.followup.send(f"This is a followup message from Button 1.")
 
-    @button(label="Button 2", style=bot.ButtonStyle.green, emoji="😊")
+    @button(label="Button 2", style=discord.ButtonStyle.green, emoji="😊")
     async def callback2(self, button, interaction):
         await interaction.response.send_message(f"Hi from Button 2")
         await interaction.followup.send(f"This is a followup message from Button 2.")
 
-    @button(label="Button 3", style=bot.ButtonStyle.red, emoji="😊")
+    @button(label="Button 3", style=discord.ButtonStyle.red, emoji="😊")
     async def callback3(self, button, interaction):
         await interaction.response.send_message(f"Hi from Button 3")
         await interaction.followup.send(f"This is a followup message from Button 3.")
 
 
-@bot.command(description="Sends the bot's latency.") # this decorator makes a slash command
-async def ping(ctx): # a slash command will be created with the name "ping"
+# this decorator makes a slash command
+@bot.command(description="Sends the bot's latency.")
+async def ping(ctx):  # a slash command will be created with the name "ping"
     await ctx.respond(f"Pong! Latency is {bot.latency}")
 
 
 @bot.command()
-async def button2(ctx): # a slash command will be created with the name "ping"
+async def button(ctx):  # a slash command will be created with the name "ping"
     await ctx.respond("Hello!", view=MyView())
+
+# eric test
+
+
+@bot.event
+async def on_message(message):
+    if message.content == "hi":
+        await message.channel.send("hello", reference=message)
+
 
 bot.run(TOKEN)
