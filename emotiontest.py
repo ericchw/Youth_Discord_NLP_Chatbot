6f00f090e22381ac2077e69b769c44252c2dfca1
Chan Kwok Fung <chankwokfung991@gmail.com>
2023-01-06 12:59:16 +0800

    emotion model

---
+++ emotiontest.py
@@ -0,0 +1,7 @@
+from transformers import pipeline
+
+emotion = pipeline('sentiment-analysis', model='arpanghoshal/EmoRoBERTa')
+
+emotion_labels = emotion("This is so stupid!")
+
+print(emotion_labels)
