290a10ce924d05c92d65bb757a66a23a00b347e1
Chan Kwok Fung <chankwokfung991@gmail.com>
2023-01-11 14:11:17 +0800

    emotion update

--- emotiontest.py
+++ emotiontest.py
@@ -9,14 +9,15 @@ from googletrans import LANGUAGES
 
 emotion = pipeline('sentiment-analysis', model='arpanghoshal/EmoRoBERTa')
 
+def transform(text):
+    # translat = google_translator()
+    translat = Translator()
+    # for lang in LANGUAGES:
+    #     print(f'{lang} - {LANGUAGES[lang]}')
+    trans = translat.translate('text')
+    # trans = translat.translate('他真的是白痴',lang_src='zh-tw', lang_tgt='en')
+    # print(trans.text)
+    emotion_labels = emotion(trans.text)
 
-# translat = google_translator()
-translat = Translator()
-# for lang in LANGUAGES:
-#     print(f'{lang} - {LANGUAGES[lang]}')
-trans = translat.translate('他真係弱智')
-# trans = translat.translate('他真的是白痴',lang_src='zh-tw', lang_tgt='en')
-# print(trans.text)
-emotion_labels = emotion(trans.text)
-
-print(emotion_labels)
+    print(emotion_labels)
+    return emotion_labels
