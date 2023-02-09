cde4caee7e38edd0156f011baa1bee236a415490
Eric Chan <48132385+ericchw@users.noreply.github.com>
2023-02-09 23:10:23 +0800

    Update dischatbot.py

--- dischatbot.py
+++ dischatbot.py
@@ -5,6 +5,8 @@ from discord.ui import Button, View, button, Modal, InputText, Select
 from emotiontest import emtransform
 import chat, faq
 from bs4 import BeautifulSoup
+from db import connectDB    
+from datetime import datetime, timezone
 
 responses= {}
 intents = discord.Intents.default()
@@ -141,33 +143,42 @@ async def on_message(message):
         # print(ans)
         if ans:
             # ans can be SQL statement for FAQ or string in intents.json, if string will output, if SQL statement will connect datebase to get data and return.
-            # print(ans.keys())
-            for key in ans.keys():
-                if key == 'sql':
-                    # print(key)
-                    val1 = connectDB(ans['sql'], "r")
-                    res = []
-                    temp = []
-                    # print(val1[1])
-                    for value in val1[1]:
-                        for x in value:
-                            print(value)
-                            temp.append(x)
-                    for key in val1[0]:
-                        print(key)
-                        for value in temp:
-                            string = key +": " + value
-                            res.append(string)
-                            temp.remove(value)
-                            break
-                    print(res)
-                    res = "\n".join(res)
-                    await message.channel.send(res)
+            # print(type(ans))
+            if type(ans) == str:
+                await message.channel.send(ans)
+                connectDB(f"INSERT INTO chatlog VALUES (DEFAULT,'{'CyberU'}', '{ans}', '{'chatbot'}', '{datetime.now(timezone.utc)}')", "u")
+            else:
+                # print(ans.keys())
+                for key in ans.keys():
+                    if key == 'sql':
+                        # print(key)
+                        val1 = connectDB(ans['sql'], "r")
+                        res = []
+                        temp = []
+                        # print(val1[1])
+                        for value in val1[1]:
+                            for x in value:
+                                #print(value)
+                                temp.append(x)
+                        for key in val1[0]:
+                            # print(key)
+                            for value in temp:
+                                string = key.replace("name","名稱").replace("phonenumber","電話").replace("whatsapp","Whatsapp").replace("website","網站").replace("instagram","Instagram").replace("discord","Discord").replace("servicehours","服務時間") + ": " + value
+                                res.append(string)
+                                temp.remove(value)
+                                break
+                        # print(res)
+                        res = "\n".join(res)
+                        await message.channel.send(res)
+                        connectDB(f"INSERT INTO chatlog VALUES (DEFAULT,'{'CyberU'}', '{res}', '{'query'}', '{datetime.now(timezone.utc)}')", "u")
+                    if key == 'serviceHours':
+                        await message.channel.send(file=discord.File(ans))
         else:
             if emotion[0]['label'] == 'anger':
                 string = "大家冷靜d"
-                # SQL: save message to database "大家冷靜d"
                 await message.channel.send(string)
+                # SQL: save message to database "大家冷靜d"
+                connectDB(f"INSERT INTO chatlog VALUES (DEFAULT,'{'CyberU'}', '{string}', '{'Solve'}', '{datetime.now(timezone.utc)}')", "u")
         # string = faq.faq(message.content)
         # if string != None:
         #     await message.channel.send(string)
