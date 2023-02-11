163e4253591902c3d01678ff9d64cc8988498997
Your Name <a.andyabcde@gmail.com>
2023-02-11 21:24:12 +0800

    Update try.py

--- try.py
+++ try.py
@@ -1,11 +1,26 @@
 import discord
 from discord.ext import commands
-
+from discord.ui import Button, View, button, Modal, InputText, Select
 
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 
+class MyView(View):
+
+    @button(label="1:Apex", style=discord.ButtonStyle.blurple)
+    async def callback1(self, button, interaction):
+        m="List:\n"+str(interaction.user)+"\nPlease select "
+        await interaction.response.edit_message(content=f"{m}", view=self)
+    @button(label="2:LOL", style=discord.ButtonStyle.green)
+    async def callback2(self, button, interaction):
+        m="List:\n"+str(interaction.user)+"\nPlease select "
+        await interaction.response.edit_message(content=f"Hi from Button 1", view=self)
+
+    @button(label="3:LOL", style=discord.ButtonStyle.red)
+    async def callback3(self, button, interaction):
+        m="List:\n"+str(interaction.user)+"\nPlease select "
+        await interaction.response.edit_message(content=f"Hi from Button 1", view=self)
 
 bot = discord.Bot(debug_guilds=["995158826347143309"], intents=intents) 
 #event
@@ -16,23 +31,8 @@ async def event(ctx):
         description="1:Apex\n2:LOL\n3:PUBG",
         color=discord.Color.red()
     )
+    await ctx.send(embed=embed)
+    await ctx.send("List:\nPlease select ", view=MyView())
 
-    message = await ctx.send(embed=embed)
-
-    await message.add_reaction("✅")
-    await message.add_reaction("❌")
-
-    def check(reaction, user):
-        return user == ctx.author and str(reaction.emoji) in ["✅", "❌"]
-
-    try:
-        reaction, user = await bot.wait_for("reaction_add", check=check, timeout=60.0)
-    except :
-        await message.clear_reactions()
-    else:
-        if str(reaction.emoji) == "✅":
-            await ctx.send("The ✅ button was pressed.")
-        elif str(reaction.emoji) == "❌":
-            await ctx.send("The ❌ button was pressed.")
 
 bot.run("OTk0ODk4OTcwMDg4MzA4NzQ2.GaEk2B.X7x5yEF1CZjHqtRM0YsMsCcSY6Qcn892V_z5Kk")
