be7a0fcbdcf0627798fa3bbb00b97db44c936119
Your Name <a.andyabcde@gmail.com>
2023-02-23 14:33:40 +0800

    chinese text

--- try.py
+++ try.py
@@ -1,45 +1,49 @@
-import discord
-from discord.ext import commands
-from discord.ui import Button, View, button, Modal, InputText, Select
+import random
+import jieba
 
-intents = discord.Intents.default()
-intents.members = True
-intents.message_content = True
-bot = discord.Bot(debug_guilds=["995158826347143309"], intents=intents) 
+# load dataset
+import json
+with open('data.json', 'r',encoding='utf-8') as f:
+    intents = json.load(f)
 
-bot.event_variable1 = ""
-bot.event_variable2 = ""
-bot.event_variable3 = ""
+# create a dictionary to store all patterns
+patterns = {}
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        # use jieba to tokenize the pattern
+        tokens = jieba.lcut(pattern)
+        for token in tokens:
+            # only consider tokens that are not stopwords
+            if token not in [' ', '\n'] and token not in patterns:
+                patterns[token] = [intent['tag']]
+            elif token in patterns:
+                patterns[token].append(intent['tag'])
 
+# define a function to get the response
+def get_response(input_text):
+    # tokenize the input text
+    tokens = jieba.lcut(input_text)
+    # find matching patterns
+    matching_patterns = []
+    for token in tokens:
+        if token in patterns:
+            matching_patterns.extend(patterns[token])
+    # select a random response from the matching patterns
+    if matching_patterns:
+        tag = random.choice(matching_patterns)
+        for intent in intents['intents']:
+            if intent['tag'] == tag:
+                response = random.choice(intent['responses'])
+                return response
+    else:
+        return "對不起，我不太明白你的意思。"
 
-class Event(View):
-    @button(label="1:Apex", style=discord.ButtonStyle.blurple)
-    async def callback1(self, button, interaction):
-        if bot.event_variable1.find(str(interaction.user))<0:
-            bot.event_variable1=bot.event_variable1+str(interaction.user)+"\n"
-        await interaction.response.edit_message(content=f"List:\n1.Apex:\n{bot.event_variable1}\n2.LOL:\n{bot.event_variable2}\n3.PUBG:\n{bot.event_variable3}\nPlease select ", view=self)
-    @button(label="2:LOL", style=discord.ButtonStyle.green)
-    async def callback2(self, button, interaction):
-        if bot.event_variable2.find(str(interaction.user))<0:
-            bot.event_variable2=bot.event_variable2+str(interaction.user)+"\n"
-        await interaction.response.edit_message(content=f"List:\n1.Apex:\n{bot.event_variable1}\n2.LOL:\n{bot.event_variable2}\n3.PUBG:\n{bot.event_variable3}\nPlease select ", view=self)
-    @button(label="3:PUBG", style=discord.ButtonStyle.red)
-    async def callback3(self, button, interaction):
-        if bot.event_variable3.find(str(interaction.user))<0:
-            bot.event_variable3=bot.event_variable3+str(interaction.user)+"\n"
-        await interaction.response.edit_message(content=f"List:\n1.Apex:\n{bot.event_variable1}\n2.LOL:\n{bot.event_variable2}\n3.PUBG:\n{bot.event_variable3}\nPlease select ", view=self)
-
-
-#event
-@bot.command(name="create_event")  #https://www.youtube.com/watch?v=56XoybDajjA&t=487s
-async def event(ctx):
-    embed = discord.Embed(
-        title="Event",
-        description="1:Apex\n2:LOL\n3:PUBG",
-        color=discord.Color.red()
-    )
-    await ctx.send(embed=embed)
-    await ctx.send("List:\nPlease select ", view=Event())
-
-
-bot.run("OTk0ODk4OTcwMDg4MzA4NzQ2.GaEk2B.X7x5yEF1CZjHqtRM0YsMsCcSY6Qcn892V_z5Kk")
+# start the chatting loop
+while True:
+    input_text = input("你好，有什麼我能幫到你的嗎？")
+    if input_text.lower() in ['bye', '再見']:
+        print("好的，下次見！")
+        break
+    else:
+        response = get_response(input_text)
+        print(response)
