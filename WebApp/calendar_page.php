3e188ef13955d8f0847ccec5d25bd2bc0f6d3e3f
Ken0723 <43611876+Ken0723@users.noreply.github.com>
2023-04-13 21:50:13 +0800

    Bot PM Log Name Bug Fix

--- WebApp/calendar_page.php
+++ WebApp/calendar_page.php
@@ -101,7 +101,7 @@ include 'checkCookie.php';
             </li>
             <li>
               <a href="botlog_page.php">
-                <i class="fa fa-comment"></i>Bot Log</a>
+                <i class="fa fa-comment"></i>Bot PM Log</a>
             </li>
             <li>
               <a href="calendar_page.php">
@@ -145,7 +145,7 @@ include 'checkCookie.php';
             </li>
             <li>
               <a href="botlog_page.php">
-                <i class="fa fa-comment"></i>Bot Log</a>
+                <i class="fa fa-comment"></i>Bot PM Log</a>
             </li>
             <li>
               <a href="calendar_page.php">
